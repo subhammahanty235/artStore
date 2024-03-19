f3860cebb959dcb264707c9aeb979ddfc5baec67
subhammahanty235 <subhammahanty235@gmail.com>
2024-03-19 21:16:37 +0530

    basic docker configurations

---
+++ backend/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Start RabbitMQ in the background
+service rabbitmq-server start
+
+# Wait until RabbitMQ is ready to accept connections
+while ! nc -z localhost 5672; do
+    echo "Waiting for RabbitMQ to start..."
+    sleep 2
+done
+
+# RabbitMQ started successfully, now run your Golang application
+echo "RabbitMQ started successfully"
+go run cmd/main.go
