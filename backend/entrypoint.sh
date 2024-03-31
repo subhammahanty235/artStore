45d6fe7aeb513800b7f079ac49b7728d2eb415f6
subhammahanty235 <subhammahanty235@gmail.com>
2024-03-31 22:16:53 +0530

    updated confs

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,11 +4,12 @@
 service rabbitmq-server start
 
 # Wait until RabbitMQ is ready to accept connections
-while ! nc -z localhost 5672; do
+while ! nc -z rabbitmq 5672; do
     echo "Waiting for RabbitMQ to start..."
     sleep 2
 done
 
 # RabbitMQ started successfully, now run your Golang application
-echo "RabbitMQ started successfully"
+echo "<-------- RabbitMQ started successfully -------->"
 go run cmd/main.go
+echo "<-------- Backend server compiler successfully ------->
