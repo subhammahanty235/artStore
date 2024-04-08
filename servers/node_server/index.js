1a6adf966890422cc902d5e371ff3b2a7b208792
subhammahanty235 <subhammahanty235@gmail.com>
2024-04-09 00:28:59 +0530

    changed backend architecture

---
+++ servers/node_server/index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!\n');
+});
+
+// Listen on port 3000
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
